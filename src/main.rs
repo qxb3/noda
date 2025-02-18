b096bcacaa110c1446a38c21c56a937c30590648
qxbthree <qxbthree@gmail.com>
2025-02-18 16:45:24 +0800

    a thing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod dbus;
+
+use dbus::NotifDbusServer;
+use std::error::Error;
+
+/// Type alias for Result.
+pub type NodaResult<T> = Result<T, Box<dyn Error>>;
+
+#[tokio::main]
+async fn main() -> NodaResult<()> {
+    NotifDbusServer::start().await?;
+
+    Ok(())
 }

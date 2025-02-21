c885e02670e4c3d22f87677ed2fea68a5d1ed38a
qxbthree <qxbthree@gmail.com>
2025-02-21 22:18:23 +0800

    Revert "remove zbus (it sucks or skill issue)"
    This reverts commit ad3c9e697ea9ea9d9938c63ad202c8b4a3464a38.

--- src/notification_server.rs
+++ src/notification_server.rs
@@ -1,13 +1,56 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, future::pending};
 
-#[derive(Debug)]
-pub struct Notification<'a> {
-    app_name: String,
-    replace_id: u32,
-    app_icon: String,
-    summary: String,
-    body: String,
-    actions: Vec<String>,
-    hints: HashMap<String, zbus::zvariant::Value<'a>>,
-    expire_timeout: i32
+use crate::NodaResult;
+
+/// Notification D-Bus Server.
+#[allow(dead_code)]
+pub struct NotificationServer {
+    notifications: Vec<()>,
+}
+
+#[zbus::interface(name = "org.freedesktop.Notifications")]
+impl NotificationServer {
+    /// When there is a new notification.
+    fn notify(
+        &mut self,
+        app_name: String,
+        _replace_id: u32,
+        _app_icon: String,
+        _summary: String,
+        _body: String,
+        _actions: Vec<String>,
+        _hints: HashMap<String, zbus::zvariant::Value<'_>>,
+        _expire_timeout: i32,
+    ) -> zbus::fdo::Result<u32> {
+        println!("{app_name}");
+        Ok(3)
+    }
+
+    /// Server info.
+    fn get_server_information(&self) -> (&str, &str, &str, &str) {
+        ("noda", "qxbthree", "0.0.1", "1.2")
+    }
+}
+
+impl NotificationServer {
+    /// Start D-Bus notification server.
+    pub async fn start() -> NodaResult<()> {
+        // Creates new dbus server.
+        let dbus_server = NotificationServer {
+            notifications: Vec::new(),
+        };
+
+        // Register D-Bus session.
+        let _conn = zbus::conn::Builder::session()?
+            .name("org.freedesktop.Notifications")?
+            .serve_at("/org/freedesktop/Notifications", dbus_server)?
+            .build()
+            .await?;
+
+        log::info!("Notification server started.");
+
+        pending::<()>().await;
+
+        Ok(())
+    }
 }
